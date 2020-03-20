0a13bd8f4482a727decb35f32e23357bdbe688b3
Jonas Hecht <jonas.hecht@codecentric.de>
2020-03-20 17:15:38 +0100

    Add paragraph 'Make Spring Boot app Graal Native Image friendly' including steps to do for GraalVM Native image execution

--- src/main/java/io/jonashackt/springbootgraal/SpringBootHelloApplication.java
+++ src/main/java/io/jonashackt/springbootgraal/SpringBootHelloApplication.java
@@ -3,7 +3,7 @@ package io.jonashackt.springbootgraal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class SpringBootHelloApplication {
 
 	public static void main(String[] args) {
