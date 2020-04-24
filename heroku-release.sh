af7e8f9ca8b922e8c997e17eec0bebb0fc9d51d7
Jonas Hecht <jonas.hecht@codecentric.de>
2020-04-24 15:12:15 +0200

    Fixing Travis curl with seperate bash script

---
+++ heroku-release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ];
+  then
+    echo "[--> ERROR] Please provide your Heroku app name as script parameter like this: ./heroku-release spring-boot-graal"
+    exit
+fi
+echo "[-->] Releasing Dockerized Heroku app '$1'"
+
+herokuAppName=$1
+dockerImageId=$(docker inspect registry.heroku.com/$herokuAppName/web --format={{.Id}})
+
+curl -X PATCH https://api.heroku.com/apps/$herokuAppName/formation \
+          -d '{
+                "updates": [
+                {
+                  "type": "web",
+                  "docker_image": "'"$dockerImageId"'"
+                }]
+              }' \
+          -H "Content-Type: application/json" \
+          -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+          -H "Authorization: Bearer $DOCKER_PASSWORD"
